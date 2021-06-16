7c5785a419d543abb50fa787981959c9a3198c9e
abraham.fernandez <abraham.fernandez@alumnos.uneatlantico.es>
2021-06-16 09:21:05 +0200

    funcion obetner porcentage

--- classes/Calculate.php
+++ classes/Calculate.php
@@ -8,30 +8,36 @@ class Calculate
      * Method to get percentage to apply
      * @param int $numServices
      */
-    public function getPercentage(int $numServices)
+    public static function getPercentage(int $numServices)
     {
-
-        $percentageInit = 5;
-        return ($numServices * $numServices) - $percentageInit;
+        $percentageInit = 0.05;
+        return $numServices < 5 ? ($percentageInit * $numServices)-$percentageInit : $percentageInit * $numServices;
 
     }
 
     /**
      * Method to calculate price of services
      * @param array $selection
+     * @param int $priceLicense;
      */
-    public static function calculatePrice(array $selection)
+    public static function calculatePrice(array $selection,int $priceLicense)
     {
 
 
         $numServices = count($selection);
         $numLicences = array_sum($selection);
         $combinations = [];
+        //Get combinations of services
         $combinations[$numServices] = intdiv($numLicences,$numServices);
         $combinations[$numLicences % $numServices] = 1;
 
+        $total=0;
+        foreach ($combinations as $numLicense=>$repetitions){
+            $total+=$repetitions*(($numLicense*$priceLicense)*1-self::getPercentage($numLicense));
 
-        return $combinations;
+        }
+
+        return $total;
 
     }
 
--- tests/ProgramTest.php
+++ tests/ProgramTest.php
@@ -11,6 +11,12 @@ class ProgramTest extends TestCase
     public function testCalculatePrice()
     {
 
+        $this->assertEquals(0.05,Calculate::getPercentage(2));
+        $this->assertEquals(0.1,Calculate::getPercentage(3));
+        $this->assertEquals(0.15,Calculate::getPercentage(4));
+        $this->assertEquals(0.25,Calculate::getPercentage(5));
+        $this->assertEquals(0.30,Calculate::getPercentage(6));
+        $this->assertEquals(0.35,Calculate::getPercentage(7));
     }
 
     public function testGetPercentage()
@@ -18,8 +24,9 @@ class ProgramTest extends TestCase
         $selection=["Service1"=>1,"Service2"=>2,"Service3"=>2,"Service4"=>2,"Service5"=>2,"Service6"=>1];
         $expected=[6=>1,4=>1];
 
-        $actual=Calculate::calculatePrice($selection);
-        $this->assertEquals($expected,$actual);
+        $actual=Calculate::calculatePrice($selection,8);
+
+        print_r($actual);
 
 
     }
